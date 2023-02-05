3ea1cfa18dff8020197c7b5d73f816d9861c903d
Iago Richard <iagorichard@outlook.com>
2023-02-05 14:47:39 -0300

    Marketing Cloud  Admin Questions

---
+++ Extração de Dados - Simulado Salesforce Marketing Cloud Admin/extraction.py
@@ -0,0 +1,79 @@
+# Coleta questões e alternativas do simulado da Certificação Salesforce Experience Cloud 
+# Consultant do Site www.freecram.net e cria um csv
+
+# Import de bibliotecas
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+questionsList      = []
+alternativesList   = []
+correctAnswersList = []
+
+primaryURL = 'https://www.freecram.net'
+URLArray = ['https://www.freecram.net/question/Salesforce.Marketing-Cloud-Administrator.v2022-12-31.q52/northern-trail-outfitters-nto-is-building-a-journey-which-randomly-sends-five-different-versions-of']
+
+for currentURL in URLArray:
+
+    # Request da página
+    response = requests.get(currentURL)
+    html     = response.content.decode()
+
+    # Criar objeto Beautiful Soup
+    bs = BeautifulSoup(html, 'html.parser')
+
+    # Questão
+    question = bs.find('div', class_='qa-question')
+    questionsList.append(question.contents[0])
+
+    # Alternativas
+    parentAlternatives = bs.find('div', class_='qa-options')
+    alternatives       = parentAlternatives.find_all('label')
+
+    tempAlt = []
+
+    for alt in alternatives:
+        tempAlt.append(alt.contents[1].string + ' ' + alt.contents[2].strip())
+    
+    alternativesList.append(tempAlt)
+
+    # Alternativa(s) correta(s)
+    parentDiv     = bs.find('div', class_='qa-answerexp')
+    correctAnswer = parentDiv.find('span')
+    correctAnswersList.append(correctAnswer.string)
+
+    # Armazena link da proxima questão
+    hrefs = bs.find_all('a',class_='btn btn-sm btn-success')
+    
+    if(len(hrefs) > 1):
+         URLArray.append(primaryURL + hrefs[1]['href'])
+    
+    elif(len(hrefs) == 1 and len(URLArray) == 1):
+         URLArray.append(primaryURL + hrefs[0]['href'])
+
+
+# Cabeçalho do arquivo .csv
+c = csv.writer(open("./Extração de Dados - Simulado Salesforce Marketing Cloud Admin/MKTCloudAdmin.csv", "a", encoding="utf-8", newline=""))
+c.writerow(["#","Questão","Alternativas", "Alternativa(s) Correta(s)"])
+
+for i in range(len(questionsList)):
+    
+    # Escreve questão
+    c.writerow([(i+1), questionsList[i]])
+
+    # Escreve alternativas e resposta correta
+    for j in range(len(alternativesList[i])):
+
+        if(j == 0):
+            c.writerow(["","",alternativesList[i][j], correctAnswersList[i]])
+        else:
+            c.writerow(["","",alternativesList[i][j]])
+
+    # Pula uma linha
+    c.writerow([])
+
+
+
+
+    
+
